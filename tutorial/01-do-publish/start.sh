d1f84ff52ff40f392ebc5bc7ad6f7cc9496d262c
Max Lapshin <max@maxidoors.ru>
2022-09-06 14:47:12 +0300

    Fix command order in cluster start script

--- tutorial/01-do-publish/start.sh
+++ tutorial/01-do-publish/start.sh
@@ -3,10 +3,10 @@
 
 read -p "Enter Flussonic license key: "  license_key
 
-kubectl create secret generic flussonic-license --from-literal=license_key="${license_key}"
-
 echo $(kubectl get secret flussonic-license -o jsonpath='{.data.license_key}' | base64 --decode)
 
 doctl kubernetes cluster create publish-01 --count 1 --region ams3
+
+kubectl create secret generic flussonic-license --from-literal=license_key="${license_key}"
 
 kubectl apply -f ./publish.yaml
