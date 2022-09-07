656c38ebd1eb9b820ae9a2681e6531980591768f
Max Lapshin <max@maxidoors.ru>
2022-09-07 14:14:51 +0300

    Fix starting of new publish and deleting load-balancer

--- tutorial/01-do-publish/stop.sh
+++ tutorial/01-do-publish/stop.sh
@@ -3,4 +3,7 @@
 kubectl delete -f publish.yaml 
 
 doctl kubernetes cluster delete publish-01 
-doctl compute load-balancer delete publish-01
+
+lb_id=$(doctl compute load-balancer list -o json | jq -r '.[] | select (.name == "publish-01") | .id')
+doctl compute load-balancer delete ${lb_id}
+
