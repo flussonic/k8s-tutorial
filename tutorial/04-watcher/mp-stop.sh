3cfd75c8071fcd384205dd6bc3135599659528ee
Max Lapshin <max@maxidoors.ru>
2023-08-15 17:49:56 +0300

    adding watcher services to this tutorial #31659

--- tutorial/04-watcher/mp-stop.sh
+++ tutorial/04-watcher/mp-stop.sh
@@ -2,8 +2,10 @@
 
 set -x
 
-multipass delete manage
+multipass delete watcher
+multipass delete central
 multipass delete streamer1
 multipass delete streamer2
+multipass delete manage
 multipass purge
 rm -f k3s.yaml
